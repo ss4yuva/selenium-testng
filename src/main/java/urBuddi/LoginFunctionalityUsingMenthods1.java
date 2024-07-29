1029e665a9b3b099e3892d96e97cb11514708cd9
Sruthi <ss4yuvan@gmail.com>
2024-07-29 10:06:09 +0530

    Initial commit of Selenium code

--- src/main/java/urBuddi/LoginFunctionalityUsingMenthods1.java
+++ src/main/java/urBuddi/LoginFunctionalityUsingMenthods1.java
@@ -5,15 +5,21 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginFunctionalityUsingMenthods1 {
 
 	static WebDriver driver;
+	static WebDriver wait;
+	
 	// Locators
 	By emailInputfield = By.id("userEmail");
 	By passwordInputfield = By.id("userPassword");
 	By loginButton = By.xpath("//*[@type='submit']");
+	By lagoutButton = By.xpath("//*[@class='logout-btn-nav']");
+	By logoutPopupYes = By.xpath("//*[text()='Yes']");
 	
 	public void loginToApplication(String userName, String password) throws InterruptedException {
 		driver.findElement(emailInputfield).click();
@@ -29,12 +35,15 @@ public class LoginFunctionalityUsingMenthods1 {
 		Thread.sleep(15000);
 	}
 	
-	public void loginWithInvalid(String userName, String password) {
+	public void loginWithInvalid(String userName, String password) throws InterruptedException {
 		driver.findElement(emailInputfield).click();
 		driver.findElement(emailInputfield).sendKeys(userName);
 		driver.findElement(passwordInputfield).click();
 		driver.findElement(passwordInputfield).sendKeys(password);
 		driver.findElement(loginButton).click();
+		Thread.sleep(1000);
+		driver.findElement(lagoutButton).click();
+		driver.findElement(logoutPopupYes).click();
 	}
 	
 	public static void main(String[] args) throws InterruptedException  {
@@ -45,7 +54,7 @@ public class LoginFunctionalityUsingMenthods1 {
 		driver.manage().window().maximize();
 		driver.get("https://dev.urbuddi.com/login");
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+		WebDriver wait = (WebDriver) new WebDriverWait(driver, Duration.ofSeconds(1000));
 	
 		LoginFunctionalityUsingMenthods1 obj=new LoginFunctionalityUsingMenthods1();
 		obj.loginToApplication("govardhan.gottemukkula@openskale.com", "Govardhan_123");
